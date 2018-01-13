ee639844d20e2f7cc678d5bab9a06fb33643da5e
Olivier Godon <olivier.godon@antinea.org>
2018-01-13 15:08:38 +0100

    HashService

---
+++ api/services/HashService.js
@@ -0,0 +1,23 @@
+/**
+ * User.js
+ *
+ * @description Users of the application
+ */
+ 
+var bcrypt = require('bcrypt');
+
+module.exports = {
+    
+  passwordHash: function(password, cb) {
+    bcrypt.hash(password, 8, function(err, hash) {
+      return cb(err, hash);
+    });
+  },
+
+  comparePasswordHash: function(password, hash, cb) {
+    bcrypt.compare(passord, hash, function(err, res) {
+      return cb(err, res);
+    });
+  }
+
+};
